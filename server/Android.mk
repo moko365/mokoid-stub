45967d534bbe68133cd27966fb01280896d4d07d
jollen <jollen@moko365.com>
2013-11-16 10:09:26 +0800

    minor update

--- server/Android.mk
+++ server/Android.mk
@@ -19,3 +19,23 @@ LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE:= ledserver
 
 include $(BUILD_EXECUTABLE)
+
+#### build ledserver_test
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	ledserver_test.cpp 
+
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libcutils \
+	libledservice
+
+LOCAL_C_INCLUDES :=  \
+	vendor/mokoid/libledservice
+
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_MODULE:= ledserver_test
+
+include $(BUILD_EXECUTABLE)
