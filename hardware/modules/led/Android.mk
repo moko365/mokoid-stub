d117ce66b39bfe92c51021acee74d294b395b770
jollen <jollen@moko365.com>
2013-11-16 10:08:32 +0800

    Modify Android.mk of Led HAL

--- hardware/modules/led/Android.mk
+++ hardware/modules/led/Android.mk
@@ -26,6 +26,6 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := led.cpp
 #LOCAL_SRC_FILES := led.cpp
-LOCAL_MODULE := led.goldfish
-#LOCAL_MODULE := led.default
+#LOCAL_MODULE := led.goldfish
+LOCAL_MODULE := led.default
 include $(BUILD_SHARED_LIBRARY)
